244aed37a79624deac6f45f41e657c75cd7e70bf
Eduardo Freitas <eduardoflorencio96@gmail.com>
2024-01-09 16:51:13 -0300

    refactor: code organization

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,34 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
 
-# aws_lambda_layer_version para criar a camada Lambda
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir  = "..\\app"
+  output_path = "${var.lambda_function_name}.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = data.archive_file.lambda.output_path
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.lambda_handler_name
+  runtime       = var.lambda_runtime
+
+  layers = [
+    aws_lambda_layer_version.my_layer.arn
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.example,
+  ]
+}
+
 resource "aws_lambda_layer_version" "my_layer" {
   layer_name          = "my_layer"
   description         = "My Lambda Layer"
@@ -39,41 +60,11 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-
-data "archive_file" "lambda" {
-  type = "zip"
-  #  source_file = "..\\app\\lambda_function.py"
-  source_dir  = "..\\app"
-  output_path = "${var.lambda_function_name}.zip"
-}
-
-resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = data.archive_file.lambda.output_path
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = var.lambda_handler_name
-  runtime       = var.lambda_runtime
-
-  layers = [
-    aws_lambda_layer_version.my_layer.arn
-  ]
-
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_logs,
-    aws_cloudwatch_log_group.example,
-  ]
-}
-
-# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
-# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
 resource "aws_cloudwatch_log_group" "example" {
   name              = "/aws/lambda/${var.lambda_function_name}"
   retention_in_days = 1
 }
 
-# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
 data "aws_iam_policy_document" "lambda_logging" {
   statement {
     effect = "Allow"
